87b68a6521624f11775feb9d424d6c4dbbc3c5a4
kodxana <romanrex19@int.pl>
2021-05-25 08:37:31 +0200

    Update settings.ts

--- src/common/settings.ts
+++ src/common/settings.ts
@@ -6,9 +6,7 @@ export interface LbrySettings {
 export const DEFAULT_SETTINGS: LbrySettings = { enabled: true, redirect: 'odysee' };
 
 export const redirectDomains = {
-  'lbry.tv': { prefix: 'https://lbry.tv/', display: 'LBRY.tv' },
   'odysee' { prefix: 'https://odysee.com/', display: 'Odysee' },
-  app: { prefix: 'lbry://', display: 'App' },
 };
 
 export function getSettingsAsync<K extends Array<keyof LbrySettings>>(...keys: K): Promise<Pick<LbrySettings, K[number]>> {
