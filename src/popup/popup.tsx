b0feca0c4117aa8c92272c91b0491f764fc4bb6f
kodxana <romanrex19@int.pl>
2021-06-29 23:00:28 +0200

    Removed po up

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -20,9 +20,6 @@ function WatchOnLbryPopup() {
     <label className='radio-label'>Enable Redirection:</label>
     <ButtonRadio value={enabled ? 'YES' : 'NO'} options={['YES', 'NO']}
       onChange={enabled => setSetting('enabled', enabled.toLowerCase() === 'yes')} />
-    <label className='radio-label'>Where would you like to redirect?</label>
-    <ButtonRadio value={redirect as string} options={redirectOptions}
-      onChange={redirect => setSetting('redirect', redirect)} />
     <label className='radio-label'>Other useful tools:</label>
     <a href='/tools/YTtoLBRY.html' target='_blank'>
       <button type='button' className='btn1 button is-primary'>Subscriptions Converter</button>
